84fa5e105db2e19179339ef61627791f9f00a9a9
samzhab <samzhab@gmail.com>
2018-10-28 00:25:22 +0200

    change param setup methods

--- deep_ai_text.rb
+++ deep_ai_text.rb
@@ -16,46 +16,69 @@ class DeepAIAPI
     puts '[deep-ai] ENTER YOU APIKEY'
   end
 
+  def setup_params(text_module, value)
+    request = {}
+    modulesset1 = %w[sentiment summarization]
+    modulesset2 = %w[tagging parmcpar]
+    if modulesset1.include?(text_module)
+      request['text'] = value
+    elsif modulesset2.include?(text_module)
+      request['sentence'] = value
+    end
+    request
+  end
+
+  def setup_headers
+    headers = {}
+    headers['api-key'] = API_KEY
+    headers
+  end
+
   def sentiment_analysis(text)
-    # posting a text url
-    response = RestClient::Request.execute(method: :post,
-                                           url: "#{API_URL}#{SENT_PATH}",
+    headers = setup_headers
+    request_payload = setup_params('sentiment', text)
+    response = RestClient::Request.execute(method:  :post,
+                                           url:     "#{API_URL}#{SENT_PATH}",
                                            timeout: 600,
-                                           headers: { 'api-key' => API_KEY },
-                                           payload: { 'text' => text })
+                                           headers: headers,
+                                           payload: request_payload)
     puts JSON.parse(response)
   end
 
   def text_summarization(text)
-    # posting a text url
-    response = RestClient::Request.execute(method: :post,
-                                           url: "#{API_URL}#{SUMM_PATH}",
+    headers = setup_headers
+    request_payload = setup_params('summarization', text)
+    response = RestClient::Request.execute(method:  :post,
+                                           url:     "#{API_URL}#{SUMM_PATH}",
                                            timeout: 600,
-                                           headers: { 'api-key' => API_KEY },
-                                           payload: { 'text' => text })
+                                           headers: headers,
+                                           payload: request_payload)
     puts JSON.parse(response)
   end
 
-  def text_tagging(sentence)
-    # posting a text url
-    response = RestClient::Request.execute(method: :post,
-                                           url: "#{API_URL}#{TAGG_PATH}",
+  def text_tagging(_sentence)
+    headers = setup_headers
+    request_payload = setup_params('tagging', text)
+    response = RestClient::Request.execute(method:  :post,
+                                           url:     "#{API_URL}#{TAGG_PATH}",
                                            timeout: 600,
-                                           headers: { 'api-key' => API_KEY },
-                                           payload: { 'sentence' => sentence })
+                                           headers: headers,
+                                           payload: request_payload)
     puts JSON.parse(response)
   end
 
-  def parsey_mc_parseface(sentence)
-    response = RestClient::Request.execute(method: :post,
-                                           url: "#{API_URL}#{PMCP_PATH}",
+  def parsey_mc_parseface(_sentence)
+    headers = setup_headers
+    request_payload = setup_params('parmcpar', text)
+    response = RestClient::Request.execute(method:  :post,
+                                           url:     "#{API_URL}#{PMCP_PATH}",
                                            timeout: 600,
-                                           headers: { 'api-key' => API_KEY },
-                                           payload: { 'sentence' => sentence })
+                                           headers: headers,
+                                           payload: request_payload)
     puts JSON.parse(response)
   end
 end
 
 deep_ai_obj = DeepAIAPI.new
-textfile = File.new('test')
+textfile = File.new('tg1')
 deep_ai_obj.sentiment_analysis(textfile)
